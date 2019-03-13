0a86eecf02347322c58405e1bd537c8acd65d81b
Kanan B <32438208+kananb@users.noreply.github.com>
2019-03-13 16:30:56 -0700

    Update Launcher.java
    Now gets port from command line

--- server/Launcher.java
+++ server/Launcher.java
@@ -2,6 +2,13 @@ package server;
 
 public class Launcher {
 	public static void main(String args[]) {
-		new Server(12345).listen();
+		int port = 12345;
+		try {
+			if (args.length >= 1) port = Integer.parseInt(args[0]);
+		} catch (NumberFormatException e) {
+			System.err.printf("The port must be a number.\nThe string \"%s\" is invalid.", args[0]);
+			System.exit(1);
+		}
+		new Server(port).listen();
 	}
 }
